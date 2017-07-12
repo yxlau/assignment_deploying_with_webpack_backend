ce0dab5e77d9cff379fb87392f47b48bee2accf6
Yi-Xuan Lau <yixuan.lau@gmail.com>
2017-07-12 11:52:45 +0800

    Limit origin

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 
 // enable CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://glacial-tundra-36350.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
